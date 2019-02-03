7976871b48069182f20264c685ec508a602e0c30
Jan Renken <jan.renken@posteo.de>
2019-02-03 16:37:59 +0100

    Introduced json file for workbench data

--- service/app.js
+++ service/app.js
@@ -1,43 +1,14 @@
 const express = require('express');
-const app = express();
-//const hostname = '127.0.0.1';
-//const port = 3000;
+const fs = require('fs');
 
-const data = [
-	{
-		"id" : 1,
-		"pid" : 0,
-		"icons" : {
-			"title": "Note!",
-			"image" : { 
-				"file" : "workbench.png",
-				"width" : 35,
-				"height" : 30
-			},
-			"imageSelected" : {
-				"file" : "workbench_selected.png",
-				"width" : 35,
-				"height" : 30
-			}
-		},
-		"window" : {
-			"title": "Note!"
-		},
-		"content" : {
-			"title": "Hello again!",
-			"articles" : [
-				{
-					"title": "Renkbench relaunch",
-					"text": "Currently lachsfilet.de is just running with static content from a node.js service, since my webhost has updated the PHP version.<br/>I took advantage of the resulting downtime to refactor my Workbench Javascript code and added some missing features like window resizing and scrolling by arrow buttons and scrollbar.<br/>A reworked backend and new content will follow soon.<br/><br/>Well, to be serious, as soon as possible. Whenever that is..."
-				}
-			]
-		}
-	}
-];
+const app = express();
 
 app.get('/', (request, response) => {
+	var file = fs.readFileSync("workbench.json");
+	var data = JSON.parse(file);
+	
 	response.set('Access-Control-Allow-Origin', '*');
 	response.json(data);
 });
 
-app.listen(process.env.PORT || 8080, () => console.log(`Example app listening on port ${process.env.PORT || 8080}!`));
+app.listen(process.env.PORT || 8080, () => console.log(`Renkbench service app listening on port ${process.env.PORT || 8080}!`));
