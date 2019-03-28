a129f3cf6a74486dc6b74800726eea27ad044e26
Jan Renken <j-renken@foni.net>
2019-03-28 23:44:44 +0100

    Cut text converter out of worbench monolith

---
+++ public/modules/text.js
@@ -0,0 +1,140 @@
+/*
+*	Just an Amiga Workbench look and feel clone, written in Javascript.
+*	Copyright 2019 by Jan Renken, Hamburg
+*	
+*	This software is licensed under the GNU General Public License Version 3.0.
+*
+*	So feel free to use, modify and distribute it.
+*
+*	Amiga for life...
+*/
+
+"use strict";
+
+// Text to image based font parser
+export var textConverter = () => {
+    //Text, charset and font properties and methods
+    var fontColor = {
+        whiteOnBlue : 0,
+        blueOnWhite : 16,
+        whiteOnOrange : 32,
+        blackOnBlue : 48,
+        blackOnWhite : 64,
+        whiteOnBlack : 80,
+        blueOnWhiteInactive : 96,
+        orangeOnBlack : 112,
+        blackOnOrange : 128
+    };
+
+    //Convert string text to amiga style font div-element-text
+    var convertText = (text, color) =>
+    {
+        //Replase some tags
+        text=text//.replace(/<\/*(br|ul) *\/*>/g,'<div class="stop"></div>')
+            .replace(/<br *\/*>/g, '<div class="stop"></div>')
+            .replace(/<ul>/g,'<div class="stop"></div><div class="list">')
+            .replace(/<\/ul>/g,'<div class="stop"></div></div>')
+            .replace(/<li>/g,'- ')
+            .replace(/<\/li>/g,'<div class="stop"></div>')
+            .replace(/<table>/g, '<div class="stop"></div><table>');
+        
+    
+        var result="";
+        var tagMode=false;
+        var word='<div class="word">';
+        var tableHeadColor=fontColor.blackOnWhite;
+        var linkColor=fontColor.whiteOnOrange;
+        var curColor=color;
+        for(var i=0; i<text.length; i++)
+        {
+            var character=text.slice(i,i+1);
+            //Check whether an html tag begins
+            tagMode=tagMode || character=="<";
+            if(tagMode)
+            {
+                if(character=="<")
+                    result=result+word+"</div>";
+                //Add tag to result without converting it
+                result=result+character;
+                //Do not convert the text until the tag stops
+                tagMode=character!=">";
+                if(tagMode)
+                    word='<div class="word">';
+                continue;
+            }
+            //Check font color to use
+            var tableHead=result.search(/<th *colspan="[0-9]*">/)!=-1
+            && result.search(/<\/th>/)==-1;
+            var link=result.search(/<a* href=".*">/)!=-1
+            && result.search(/<\/a>/)==-1;
+            if(tableHead)
+                curColor=tableHeadColor;
+            else if(link)
+                curColor=linkColor;
+            else
+                curColor=color;
+            //Get Amiga font div element equivalent to the passed character
+            var element=parseChar(character, curColor, "text");
+            //Add the element to the current word
+            if(element)
+                word=word+element;
+            //Add current word to resultset and create a new word in 
+            //case of a space character
+            if(character==" ")
+            {
+                result=result+word+"</div>";
+                word='<div class="word">';			  
+            }		
+        }
+        result=result+word+'</div><div class="stop"></div>';
+    //console.debug(result);		
+        return result;
+    };
+
+    //Test method for string -> charset image conversion
+    var parseChar = (character, color, mode) =>
+    {
+        var xCoord=getCharIndex(character);
+    //console.debug("Char: %s, Mapping: %i", character, xCoord);
+        if(typeof xCoord!=="number")
+            return null;
+        if(typeof color!=="number")
+            return null;
+        if(mode==="text")
+            return '<div class="char" data-char="'+ character +
+                '" style="background-position: -'+xCoord+'px -'
+                +color+'px"></div>';
+    };
+
+    //Charset mapping for topaz font
+    var getCharIndex = character =>
+    {
+        var charsetMapping = [
+            " ", "0", "1", "2", "3", "4", "5", "6",	"7", "8", "9",
+            "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k",
+            "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v",
+            "w", "x", "y", "z", "\u00e4", "\u00f6", "\u00fc", "\u00df", "\u00e1", "\u00e0", "\u00e2",
+            "\u00e9", "\u00e8", "\u00ea", "\u00f3", "\u00f2", "\u00f4", "\u00e6", "A", "B", "C", "D",
+            "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O",
+            "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
+            "\u00c4", "\u00d6", "\u00dc", "\u00c1", "\u00c0", "\u00c3", "\u00c9", "\u00c8", "\u00ca", "\u00d3", "\u00d2",
+            "\u00d4", "\u00c6", "^", "°", "!", "\"", "\u00a7", "$", "%", "&", "/",
+            "(", ")", "=", "?", "'", "`", "+", "*", "~", "#", "'",
+            "-", "_", ".", ":", ",", ";", "\u00b5", "<", ">", "|", "[",
+            "]", "@", "\u00b2", "\u00b3", "\\", "{", "}"
+        ];
+        
+        for(var index in charsetMapping)
+        {
+            if(character===charsetMapping[index])
+            return index*8;
+        }
+        return 0;
+    };
+
+    return {
+        fontColor : fontColor,
+        convertText : convertText,
+        parseChar, parseChar
+    };
+};
--- public/workbench.js
+++ public/workbench.js
@@ -12,6 +12,7 @@
 "use strict";
 
 import {createNode} from "./modules/domTree.js";
+import {textConverter} from "./modules/text.js";
 
 //The workbench main object
 (() => {
@@ -83,7 +84,7 @@ import {createNode} from "./modules/domTree.js";
 		// Set version and build numbers
 		createVersionInfo(version, build, release);
 
-		var title = convertText(MAIN_TITLE, fontColor["blueOnWhite"]);
+		var title = textConverter().convertText(MAIN_TITLE, textConverter().fontColor["blueOnWhite"]);
 		var mainTitle = document.getElementById("mainTitle");
 		createNode("div").style({
 				marginTop:"2px",
@@ -249,7 +250,7 @@ import {createNode} from "./modules/domTree.js";
 				getNode();
 				
 				// Icon title 
-				var text = convertText(this.title, fontColor["whiteOnBlue"]);
+				var text = textConverter().convertText(this.title, textConverter().fontColor["whiteOnBlue"]);
 				var textImage = createNode("div").style({
 					marginTop:"2px",
 				}).innerHtml(text)
@@ -398,8 +399,8 @@ import {createNode} from "./modules/domTree.js";
 				*		<div class="fillerWhiteRight" />
 				*	</div>
 				*/
-				var title = convertText(this.name, fontColor["blueOnWhite"]);
-				var titleInactive = convertText(this.name, fontColor["blueOnWhiteInactive"]);
+				var title = textConverter().convertText(this.name, textConverter().fontColor["blueOnWhite"]);
+				var titleInactive = textConverter().convertText(this.name, textConverter().fontColor["blueOnWhiteInactive"]);
 				
 				var titleBar = createNode("div").class("titleBar").appendTo(this.element).getNode();
 				createNode("div").class("fillerWhiteLeft").appendTo(titleBar);
@@ -564,7 +565,7 @@ import {createNode} from "./modules/domTree.js";
 					return false;
 
 				// Set the content title
-				createNode("div").class("text").innerHtml(convertText(content.title, fontColor["whiteOnBlue"])).appendTo(contentElement);
+				createNode("div").class("text").innerHtml(textConverter().convertText(content.title, textConverter().fontColor["whiteOnBlue"])).appendTo(contentElement);
 				createNode("div").class("stop").appendTo(contentElement);
 														
 				// Add raw text for search engines and readers
@@ -615,7 +616,7 @@ import {createNode} from "./modules/domTree.js";
 						if(contentType=="images")
 							continue;
 						
-						var text=convertText(article[contentType], fontColor["whiteOnBlue"]);
+						var text=textConverter().convertText(article[contentType], textConverter().fontColor["whiteOnBlue"]);
 						
 						// Add raw text for search engines and readers
 						var rawText = createNode(contentType === "title" ? "h2" : "p").innerHtml(article[contentType]).getNode();
@@ -929,7 +930,7 @@ import {createNode} from "./modules/domTree.js";
 				if(!this.insertMode)
 					this.removeChar();
 
-				var character = parseChar(event.key, fontColor.whiteOnBlue, "text");
+				var character = textConverter().parseChar(event.key, textConverter().fontColor.whiteOnBlue, "text");
 				this.activeForm.innerHTML = this.activeForm.innerHTML + character;
 				var lastChar = this.activeForm.childNodes[this.activeForm.childNodes.length-1];
 				lastChar.style.position = "absolute";
@@ -972,7 +973,7 @@ import {createNode} from "./modules/domTree.js";
 				if(cursor.childNodes.length > 0)
 					cursor.removeChild(cursor.firstChild);
 				if(character.length > 0)
-					cursor.innerHTML = parseChar(character[0].dataset.char, fontColor.blackOnOrange, "text");
+					cursor.innerHTML = textConverter().parseChar(character[0].dataset.char, textConverter().fontColor.blackOnOrange, "text");
 			},
 
 			removeChar : function() {
@@ -999,7 +1000,7 @@ import {createNode} from "./modules/domTree.js";
 			{
 				var item = items[itemIndex];
 				var dropdown = createNode("div").class("dropdown").appendTo(menu).getNode();
-				var title = convertText(item.name, fontColor.blueOnWhite);
+				var title = textConverter().convertText(item.name, textConverter().fontColor.blueOnWhite);
 				createNode("button").class("dropdown-title").appendTo(dropdown).innerHtml(title).getNode();
 				
 				var content = createNode("div").class("dropdown-content").appendTo(dropdown).getNode();
@@ -1007,7 +1008,7 @@ import {createNode} from "./modules/domTree.js";
 				{
 					var entry = item.entries[entryId];
 					var enabled = entry.conditions === "true";			
-					var text = convertText(entry.name, enabled ? fontColor.blueOnWhite : fontColor.blueOnWhiteInactive);
+					var text = textConverter().convertText(entry.name, enabled ? textConverter().fontColor.blueOnWhite : textConverter().fontColor.blueOnWhiteInactive);
 					createNode("div").class(enabled ? "dropdown-entry" : "dropdown-entry-disabled")
 						.appendTo(content)
 						.innerHtml(text)
@@ -1036,7 +1037,7 @@ import {createNode} from "./modules/domTree.js";
 
 				var isEnabled = enabled && enable && checkConditions(child.dataset.conditions);
 				child.className = isEnabled ? "dropdown-entry": "dropdown-entry-disabled";
-				setMenuEntryColor(child, isEnabled ? fontColor.blueOnWhite : fontColor.blueOnWhiteInactive);
+				setMenuEntryColor(child, isEnabled ? textConverter().fontColor.blueOnWhite : textConverter().fontColor.blueOnWhiteInactive);
 				child.dataset.isEnabled = isEnabled;
 			}
 		};
@@ -1620,7 +1621,7 @@ import {createNode} from "./modules/domTree.js";
 		return false;
 	};
 
-	var hoverMenuEntry = menuEntry => switchMenuEntryColor(menuEntry, fontColor.orangeOnBlack, fontColor.blueOnWhite);
+	var hoverMenuEntry = menuEntry => switchMenuEntryColor(menuEntry, textConverter().fontColor.orangeOnBlack, textConverter().fontColor.blueOnWhite);
 
 	var switchMenuEntryColor = (menuEntry, fromColor, toColor) => {
 		for(var i = 0; i < menuEntry.children.length; i++)
@@ -1960,132 +1961,13 @@ import {createNode} from "./modules/domTree.js";
 	var createVersionInfo = (version, build, release) => {
 		var text = "Renkbench version " + version + " Release " + release + " Build " + build;
 		var info = document.getElementById("info-bar");
-		var textNode = convertText(text, fontColor.blueOnWhite);
+		var textNode = textConverter().convertText(text, textConverter().fontColor.blueOnWhite);
 		createNode("div").style({
 			marginTop:"2px",
 		}).innerHtml(textNode)
 		.appendTo(info);
 	};
-	
-	//Text, charset and font properties and methods
-	var fontColor = {
-		whiteOnBlue : 0,
-		blueOnWhite : 16,
-		whiteOnOrange : 32,
-		blackOnBlue : 48,
-		blackOnWhite : 64,
-		whiteOnBlack : 80,
-		blueOnWhiteInactive : 96,
-		orangeOnBlack : 112,
-		blackOnOrange : 128
-	};
-	
-	//Convert string text to amiga style font div-element-text
-	var convertText = (text, color) =>
-	{
-		//Replase some tags
-		text=text//.replace(/<\/*(br|ul) *\/*>/g,'<div class="stop"></div>')
-			.replace(/<br *\/*>/g, '<div class="stop"></div>')
-			.replace(/<ul>/g,'<div class="stop"></div><div class="list">')
-			.replace(/<\/ul>/g,'<div class="stop"></div></div>')
-			.replace(/<li>/g,'- ')
-			.replace(/<\/li>/g,'<div class="stop"></div>')
-			.replace(/<table>/g, '<div class="stop"></div><table>');
 		
-	  
-		var result="";
-		var tagMode=false;
-		var word='<div class="word">';
-		var tableHeadColor=fontColor["blueOnWhite"];
-		var linkColor=fontColor["whiteOnOrange"];
-		var curColor=color;
-		for(var i=0; i<text.length; i++)
-		{
-			var character=text.slice(i,i+1);
-			//Check whether an html tag begins
-			tagMode=tagMode || character=="<";
-			if(tagMode)
-			{
-				if(character=="<")
-					result=result+word+"</div>";
-				//Add tag to result without converting it
-				result=result+character;
-				//Do not convert the text until the tag stops
-				tagMode=character!=">";
-				if(tagMode)
-					word='<div class="word">';
-				continue;
-			}
-			//Check font color to use
-			var tableHead=result.search(/<th *colspan="[0-9]*">/)!=-1
-			  && result.search(/<\/th>/)==-1;
-			var link=result.search(/<a* href=".*">/)!=-1
-			  && result.search(/<\/a>/)==-1;
-			if(tableHead)
-				curColor=tableHeadColor;
-			else if(link)
-				curColor=linkColor;
-			else
-				curColor=color;
-			//Get Amiga font div element equivalent to the passed character
-			var element=parseChar(character, curColor, "text");
-			//Add the element to the current word
-			if(element)
-				word=word+element;
-			//Add current word to resultset and create a new word in 
-			//case of a space character
-			if(character==" ")
-			{
-				result=result+word+"</div>";
-				word='<div class="word">';			  
-			}		
-		}
-		result=result+word+'</div><div class="stop"></div>';
-//console.debug(result);		
-		return result;
-	};
-	
-	//Test method for string -> charset image conversion
-	var parseChar = (character, color, mode) =>
-	{
-		var xCoord=getCharIndex(character);
-//console.debug("Char: %s, Mapping: %i", character, xCoord);
-		if(typeof xCoord!=="number")
-			return null;
-		if(typeof color!=="number")
-			return null;
-		if(mode==="text")
-			return '<div class="char" data-char="'+ character +
-				'" style="background-position: -'+xCoord+'px -'
-				+color+'px"></div>';
-	};
-	
-	//Charset mapping for topaz font
-	var getCharIndex = character =>
-	{
-		var charsetMapping = [
-			" ", "0", "1", "2", "3", "4", "5", "6",	"7", "8", "9",
-			"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k",
-			"l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v",
-			"w", "x", "y", "z", "\u00e4", "\u00f6", "\u00fc", "\u00df", "\u00e1", "\u00e0", "\u00e2",
-			"\u00e9", "\u00e8", "\u00ea", "\u00f3", "\u00f2", "\u00f4", "\u00e6", "A", "B", "C", "D",
-			"E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O",
-			"P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
-			"\u00c4", "\u00d6", "\u00dc", "\u00c1", "\u00c0", "\u00c3", "\u00c9", "\u00c8", "\u00ca", "\u00d3", "\u00d2",
-			"\u00d4", "\u00c6", "^", "°", "!", "\"", "\u00a7", "$", "%", "&", "/",
-			"(", ")", "=", "?", "'", "`", "+", "*", "~", "#", "'",
-			"-", "_", ".", ":", ",", ";", "\u00b5", "<", ">", "|", "[",
-			"]", "@", "\u00b2", "\u00b3", "\\", "{", "}"
-		];
-		
-		for(var index in charsetMapping)
-		{
-			if(character===charsetMapping[index])
-			   return index*8;
-		}
-		return 0;
-	};
-
 	var core = {
 		version : () => {
 			var infoNode = document.getElementById("info-bar");
