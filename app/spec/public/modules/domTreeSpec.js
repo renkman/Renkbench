f0b15c77d3990bae29de8296d93ca18cad26fc66
Jan Renken <jan.renken@posteo.de>
2022-11-05 18:43:55 +0100

    Remove Node.js backend, replace Window with jsdom and add frontend test dockerfile

--- app/spec/public/modules/domTreeSpec.js
+++ app/spec/public/modules/domTreeSpec.js
@@ -1,4 +1,4 @@
-import Window from "window";
+import jsdom from "jsdom";
 import {createNode} from "../../../public/modules/domTree.js";
 
 describe("createNode tests", function() {
@@ -11,7 +11,7 @@ describe("createNode tests", function() {
     });
 
     it("createNode() returns a builder object", function() {
-        const { document } = new Window();
+        const { document } = new jsdom.JSDOM(`<body></body>`).window;
         var create = createNode("div", document);
         
         expect(create).not.toBe(null);
@@ -19,7 +19,7 @@ describe("createNode tests", function() {
     });
 
     it("createNode.getNode() returns a builder object", function() {
-        const { document, HTMLElement } = new Window();
+        const { document, HTMLElement } = new jsdom.JSDOM(`<body></body>`).window;
         var node = createNode("div", document).getNode();
         
         expect(node).not.toBe(null);
@@ -27,28 +27,28 @@ describe("createNode tests", function() {
     });
 
     it("createNode.class() sets the class of the node", function() {
-        const { document } = new Window();
+        const { document } = new jsdom.JSDOM(`<body></body>`).window;
         var node = createNode("div", document).class("Machine Head").getNode();
         
         expect(node.className).toBe("Machine Head");
     });
 
     it("createNode.id() sets the id of the node", function() {
-        const { document } = new Window();
+        const { document } = new jsdom.JSDOM(`<body></body>`).window;
         var node = createNode("div", document).id("Carcass").getNode();
         
         expect(node.id).toBe("Carcass");
     });
     
     it("createNode.data() sets the data of the node", function() {
-        const { document } = new Window();
+        const { document } = new jsdom.JSDOM(`<body></body>`).window;
         var node = createNode("div", document).data({band: "At The Gates"}).getNode();
         
         expect(node.dataset.band).toBe("At The Gates");
     });
 
     it("createNode.innerHtml() sets the child of the node", function() {
-        const { document, HTMLElement  } = new Window();
+        const { document, HTMLElement } = new jsdom.JSDOM(`<body></body>`).window;
         var node = createNode("div", document).innerHtml("<div>Death</div>").getNode();
        
         expect(node.firstChild).toEqual(jasmine.any(HTMLElement));
@@ -56,21 +56,21 @@ describe("createNode tests", function() {
     });
 
     it("createNode.style() sets the style of the node", function() {
-        const { document } = new Window();
+        const { document } = new jsdom.JSDOM(`<body></body>`).window;
         var node = createNode("div", document).style({backgroundColor: "#000000"}).getNode();
         
         expect(node.style.backgroundColor).toBe("rgb(0, 0, 0)");
     });
 
     it("createNode.tabIndex() sets the tab index of the node", function() {
-        const { document } = new Window();
+        const { document } = new jsdom.JSDOM(`<body></body>`).window;
         var node = createNode("div", document).tabIndex(42).getNode();
         
         expect(node.tabIndex).toBe(42);
     });
 
     it("createNode.clone() clones the node", function() {
-        const { document, HTMLElement } = new Window();
+        const { document } = new jsdom.JSDOM(`<body></body>`).window;
         var create1 = createNode("div", document).innerHtml("Kreator");
         var create2 = create1.clone();
         var node1 = create1.getNode();
@@ -81,7 +81,7 @@ describe("createNode tests", function() {
     });
 
     it("createNode.clone(deep) clones the node and its decendants", function() {
-        const { document } = new Window();
+        const { document } = new jsdom.JSDOM(`<body></body>`).window;
         var create1 = createNode("div", document).innerHtml("Bloodbath");
         var create2 = create1.clone(true);
         var node1 = create1.getNode();
@@ -92,7 +92,7 @@ describe("createNode tests", function() {
     });
 
     it("createNode.appendTo() sets the parent node of the node", function() {
-        const { document } = new Window();
+        const { document } = new jsdom.JSDOM(`<body></body>`).window;
         var parent = createNode("div", document).getNode();
         var node = createNode("div", document).appendTo(parent).getNode();
         
@@ -100,7 +100,7 @@ describe("createNode tests", function() {
     });
 
     it("createNode.append() sets the child node of the node", function() {
-        const { document } = new Window();
+        const { document } = new jsdom.JSDOM(`<body></body>`).window;
         var child = createNode("div", document).getNode();
         var node = createNode("div", document).append(child).getNode();
         
